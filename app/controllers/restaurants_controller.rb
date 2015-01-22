43e4bc04ca24cf98ec966eb02042dd8258f5e79c
Jodi Collins <jodicollins95@gmail.com>
2015-01-22 10:29:07 -0500

    Set user permission

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,24 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:search, :index, :show]
+  before_action :check_user, except: [:search,  :index, :show]
 
-  respond_to :html
-
-  def index
-    @restaurants = Restaurant.all
-    respond_with(@restaurants)
+  def search
+    if params[:search].present?
+      @restaurants = Restaurant.search(params[:search])
+    else
+      @restaurants = Restaurant.all
+    end
   end
 
+  # GET /restaurants
+  # GET /restaurants.json
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/1
+  # GET /restaurants/1.json
   def show
     @reviews = Review.where(restaurant_id: @restaurant.id).order("created_at DESC")
     if @reviews.blank?
@@ -17,35 +28,68 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
-    respond_with(@restaurant)
   end
 
+  # GET /restaurants/1/edit
   def edit
   end
 
+  # POST /restaurants
+  # POST /restaurants.json
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    respond_with(@restaurant)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /restaurants/1
+  # PATCH/PUT /restaurants/1.json
   def update
-    @restaurant.update(restaurant_params)
-    respond_with(@restaurant)
+    respond_to do |format|
+      if @restaurant.update(restaurant_params)
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
+        format.json { render :show, status: :ok, location: @restaurant }
+      else
+        format.html { render :edit }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /restaurants/1
+  # DELETE /restaurants/1.json
   def destroy
     @restaurant.destroy
-    respond_with(@restaurant)
+    respond_to do |format|
+      format.html { redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
       @restaurant = Restaurant.find(params[:id])
     end
 
+    def check_user
+      unless current_user.admin?
+        redirect_to root_url, alert: "Sorry, only admins can do that!"
+      end
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
       params.require(:restaurant).permit(:name, :address, :phone, :website, :image)
     end
